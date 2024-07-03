a0be50423a716c0e9aaba9db91ac7446cbd513f0
ArtemArmash <artemkraft3@gmail.com>
2024-07-03 13:52:59 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+
+let name = "Artem"; 
+console.log(name);
+
+let age = 16; 
+console.log(age);
+
+let birthYear = 2007; 
+console.log(birthYear);
+
+
+let userName = prompt("Введіть ваше ім'я:");
+console.log(userName);
+
+let userAgree = confirm("Ви погоджуєтесь з умовою?");
+if (userAgree) {
+    console.log("Користувач погодився з умовою.");
+} else {
+    console.log("Користувач не погодився з умовою.");
+}
